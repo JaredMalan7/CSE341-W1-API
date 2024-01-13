be51e2e5ba19e41e2cee2204450edd2b2d9c1da9
Jared Malan <jared.malan7@gmail.com>
2024-01-13 12:10:21 -0700

    connection to the api fully completed

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const routes = require('express').Router()
-const myController = require('../controllers')
-
-routes.get('/professional', myController.testProfessionalData)
-
-module.exports = routes
--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require('express');
+const fs = require('fs')
+const app = express();
+const port = process.env.PORT || 8080;
+
+const professionalData = {
+    professionalName: 'Jared Malan',
+    base64Image: base64Encode('professional-photo.JPG'),
+    nameLink: {
+        firstName: 'Jared ',
+        url: '',
+    },
+    primaryDescription: 'Full Stack Developer',
+    workDescription1: 'Experience in web development...',
+    workDescription2: 'Skilled in JavaScript, HTML, CSS...',
+    linkTitleText: 'Links:',
+    linkedInLink: {
+        text: 'LinkedIn',
+        link: 'https://linkedin.com/in/jaredmalan7',
+    },
+    githubLink: {
+        text: 'GitHub',
+        link: 'https://github.com/JaredMalan7',
+    },
+}
+
+function base64Encode(file) {
+    const bitmap = fs.readFileSync(file)
+    return Buffer.from(bitmap).toString('base64')
+}
 
 
-app.use('/', require('./routes'))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+app.get('/professional', (req, res) => {
+    res.json(professionalData)
+})
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
